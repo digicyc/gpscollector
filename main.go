30086bac2257eba7c4f1048e7b253dab3f839489
AaronA <digicyc@gmail.com>
2025-04-08 18:38:09 -0600

    Insert and Query from MongoDB. Correct datatypes on GPS data.

--- main.go
+++ main.go
@@ -2,27 +2,40 @@ package main
 
 import (
     "net/http"
+    "os"
+    "log"
 
+    "go.mongodb.org/mongo-driver/bson"
     "github.com/gin-gonic/gin"
     //"github.com/google/uuid"
 )
 
 type gps struct {
-    ID         string  `json:"id"`
-    Device     string  `json:"device"`
-    Latitude   string  `jsoon:"latitude"`
-    Longitude  string  `json:"longitude"`
-    Elevation  string  `json:"elevation"`
-    Time       string  `json:"time"`
+    DevID      string  `json:"devid" bson:"devid"`
+    Model      string  `json:"model" bson:"model"`
+    Latitude   float64 `jsoon:"latitude" bson:"latitude"`
+    Longitude  float64 `json:"longitude" bson:"longitude"`
+    Elevation  float64 `json:"elevation" bson:"elevation"`
+    TimeStamp  string  `json:"timestamp" bson:"timestamp"`
 }
 
 var gps_data = []gps{
-    {ID: "1", Device: "model1", Latitude: "47.407614681869745", 
-     Longitude: "8.553115781396627", Elevation: "451.79998779296875",
-     Time: "2015-11-13T12:57:24.000Z"},
+    {DevID: "1", Model: "model1", Latitude: 47.407614681869745, 
+     Longitude: 8.553115781396627, Elevation: 451.79998779296875,
+     TimeStamp: "2015-11-13T12:57:24.000Z"},
+}
+
+
+func getUri() string {
+    var uri string
+    if uri = os.Getenv("MONGODB_URI"); uri == "" {
+        return "mongodb://localhost:27017"
+    }
+    return uri
 }
 
 func getGPS(c *gin.Context) {
+    // Get GPS from database
     c.IndentedJSON(http.StatusOK, gps_data)
 }
 
@@ -31,27 +44,45 @@ func postGPS(c *gin.Context) {
     var newGPS gps
     //uid := uuid.New()
 
-    // Call BindJSON to bind the received JSON to
-    // newGPS
     if err := c.BindJSON(&newGPS); err != nil {
-        return
+        log.Fatal(err)
+        return 
     }
-
-    gps_data = append(gps_data, newGPS)
+    uri := getUri()
+    client, ctx, cancel, err := MongoConnect(uri)
+    if err != nil {
+        panic(err)
+    }
+    //gps_data = append(gps_data, newGPS)
+    InsertOne(client, ctx, "gpscollector", "gpsdata", newGPS)
     c.IndentedJSON(http.StatusCreated, newGPS)
+    MongoClose(client, ctx, cancel)
 }
 
 
 func getGPSByID(c *gin.Context) {
     id := c.Param("id")
 
-    for _, a := range gps_data {
-        if a.ID == id {
-            c.IndentedJSON(http.StatusOK, a)
-            return
-        }
+    client, ctx, cancel, err := MongoConnect(getUri())
+    if err != nil {
+        panic(err)
     }
-    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "gps data not found"})
+
+    filter := bson.D{{"devid", id}}
+    option := bson.D{{"_id", 0}}
+    cursor, err := MongoQuery(client, ctx, "gpscollector", "gpsdata", filter, option)
+    if err != nil {
+        panic(err)
+    }
+    //var results []bson.D
+    var results []gps
+    if err := cursor.All(ctx, &results); err != nil {
+        panic(err)
+    }
+
+    c.IndentedJSON(http.StatusOK, results)
+    //c.IndentedJSON(http.StatusNotFound, gin.H{"message": "gps data not found"})
+    MongoClose(client, ctx, cancel)
 }
 
 
